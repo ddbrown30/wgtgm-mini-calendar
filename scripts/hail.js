9a75e478066b4e1fc61e8e81a270e43934feaa85
Stephen Garrett <42524322+xthesaintx@users.noreply.github.com>
2025-12-12 12:49:01 +1300

    2.1

--- scripts/hail.js
+++ scripts/hail.js
@@ -9,13 +9,8 @@ export class HailWeatherEffect extends foundry.canvas.containers.ParticleEffect 
    * @type {PIXI.particles.EmitterConfigV3}
    */
   static HAIL_CONFIG = {
-    lifetime: { min: 1.5, max: 2.0 }, // Constant lifetime
-    frequency: 0.004, 
-    spawnChance: 1,
-    particlesPerWave: 1,
-    emitterLifetime: -1,
-    pos: { x: 0, y: 0 },
-    addAtBack: false,
+    lifetime: { min: 5, max: 5 }, // Constant lifetime
+
     behaviors: [
       {
         type: "alpha",
@@ -35,27 +30,12 @@ export class HailWeatherEffect extends foundry.canvas.containers.ParticleEffect 
         }
       },
       {
-        type: "rotationStatic", 
-        config: {
-          min: 90,
-          max: 90
-        }
-      },
-      {
         type: "scale",
         config: {
           scale: {
             list: [{time: 0, value: 0.35}, {time: 1, value: 0.15}]
           },
           minMult: 0.3
-        }
-      },
-      {
-        type: "color",
-        config: {
-          color: {
-            list: [{time: 0, value: "#ffffff"}, {time: 1, value: "#dceeff"}]
-          }
         }
       },
       {
@@ -79,7 +59,7 @@ export class HailWeatherEffect extends foundry.canvas.containers.ParticleEffect 
   getParticleEmitters() {
     const d = canvas.dimensions;
     
-    const maxParticles = (d.width / d.size) * (d.height / d.size) * 1.5;
+    const maxParticles = (d.width / d.size) * (d.height / d.size) * .25;
     
     const config = foundry.utils.deepClone(this.constructor.HAIL_CONFIG);
     config.maxParticles = maxParticles;
